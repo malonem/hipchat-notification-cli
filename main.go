910d3152c9d3da5d53c18ef32a6dd2bc73c6e23a
Mark Malone <mark.malone@orbitz.com>
2015-09-30 17:11:56 -0500

    parameterize

--- main.go
+++ main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"bytes"
+	"flag"
 	"fmt"
 	"github.com/tbruyelle/hipchat-go/hipchat"
 	"io"
@@ -9,25 +10,38 @@ import (
 )
 
 func main() {
+	token := flag.String("token", "", "authentication token")
+	room := flag.String("room", "", "room name")
+	color := flag.String("color", "yellow", "message background color: green, red, purple, gray, random")
+	notify := flag.Bool("notify", false, "notify users")
+	format := flag.String("format", "text", "format rendered: text or html")
+
+	flag.Parse()
+
 	buf := bytes.NewBuffer(nil)
 	f := os.Stdin
-	fi, err := f.Stat()
-	if err != nil {
-		panic(err)
+	var s string
+	if f != nil {
+		fi, err := f.Stat()
+		if err != nil {
+			panic(err)
+		}
+		if fi.Mode()&os.ModeNamedPipe != 0 {
+			io.Copy(buf, f)
+			f.Close()
+		}
+		s = string(buf.Bytes())
 	}
-	if fi.Mode()&os.ModeNamedPipe != 0 {
-		io.Copy(buf, f)
-		f.Close()
-	}
-	s := string(buf.Bytes())
 
-	c := hipchat.NewClient("yCacsX0NsiniGeh0AkL0rmUdSxaLn1Pxz7YrTu00")
-	notifRq := &hipchat.NotificationRequest{Message: s, Color: "purple", Notify: false, MessageFormat: "text"}
+	if len(*token) > 0 && len(*room) > 0 {
+		c := hipchat.NewClient(*token)
+		notifRq := &hipchat.NotificationRequest{Message: s, Color: *color, Notify: *notify, MessageFormat: *format}
 
-	resp, err := c.Room.Notification("m-test", notifRq)
-	if err != nil {
-		fmt.Fprintf(os.Stderr, "Error during room notification %q\n", err)
-		fmt.Fprintf(os.Stderr, "Server returns %+v\n", resp)
-		return
+		resp, err := c.Room.Notification(*room, notifRq)
+		if err != nil {
+			fmt.Fprintf(os.Stderr, "Error during room notification %q\n", err)
+			fmt.Fprintf(os.Stderr, "Server returns %+v\n", resp)
+			return
+		}
 	}
 }
