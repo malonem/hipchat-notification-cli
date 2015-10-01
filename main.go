996b44a1e1de3afe8da82455b870dd7fe11723a7
Mark Malone <mark.malone@orbitz.com>
2015-10-01 18:07:05 -0500

    refactor

--- main.go
+++ main.go
@@ -9,45 +9,65 @@ import (
 	"os"
 )
 
-func main() {
-	token := flag.String("token", "", "authentication token")
-	room := flag.String("room", "", "room name")
-	color := flag.String("color", "yellow", "message background color: green, red, purple, gray, random")
-	notify := flag.Bool("notify", false, "notify users")
-	format := flag.String("format", "text", "format rendered: text or html")
-	message := flag.String("message", "", "message to send")
+var (
+	token   = flag.String("token", "", "authentication token")
+	room    = flag.String("room", "", "room name")
+	color   = flag.String("color", "yellow", "message background color: green, red, purple, gray, random")
+	notify  = flag.Bool("notify", false, "notify users")
+	format  = flag.String("format", "text", "format rendered: text or html")
+	message = flag.String("message", "", "message to send")
+)
 
-	flag.Parse()
-
+func readMessageFromStdin() (*string, error) {
 	buf := bytes.NewBuffer(nil)
 	stdin := os.Stdin
 	if stdin != nil {
 		pipeFile, err := stdin.Stat()
 		if err != nil {
-			panic(err)
+			return nil, err
 		}
 		if pipeFile.Mode()&os.ModeNamedPipe != 0 {
 			io.Copy(buf, stdin)
 			stdin.Close()
 		}
-		stdin_message := string(buf.Bytes())
-		if len(stdin_message) > 0 {
-			message = &stdin_message
+		stdinMessage := string(buf.Bytes())
+		if len(stdinMessage) > 0 {
+			return &stdinMessage, nil
 		}
 	}
+	return nil, nil
+}
 
-	if len(*token) > 0 && len(*room) > 0 && len(*message) > 0 {
+func isValidRequest(t *string, r *string, m *string) bool {
+	return len(*t) > 0 && len(*r) > 0 && len(*m) > 0
+}
+
+func main() {
+
+	flag.Parse()
+
+	notification, _ := readMessageFromStdin()
+	if notification == nil {
+		notification = message
+	}
+
+	if isValidRequest(token, room, notification) {
 		client := hipchat.NewClient(*token)
-		notifRq := &hipchat.NotificationRequest{Message: *message, Color: *color, Notify: *notify, MessageFormat: *format}
 
-		resp, err := client.Room.Notification(*room, notifRq)
+		request := hipchat.NotificationRequest{
+			Message:       *notification,
+			Color:         *color,
+			Notify:        *notify,
+			MessageFormat: *format,
+		}
+
+		response, err := client.Room.Notification(*room, &request)
 		if err != nil {
 			fmt.Fprintf(os.Stderr, "Error during room notification %q\n", err)
-			fmt.Fprintf(os.Stderr, "Server returns %+v\n", resp)
+			fmt.Fprintf(os.Stderr, "Server returns %+v\n", response)
 			return
 		}
 	} else {
 		fmt.Fprintln(os.Stderr, "Message, Token, and Room must be specified.")
 	}
-
 }
