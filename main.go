e80cfaa9a7da43478a035b1326bba3d9c95ce89e
Mark Malone <mark.malone@orbitz.com>
2015-10-01 16:47:25 -0500

    Add -message option

--- main.go
+++ main.go
@@ -15,12 +15,12 @@ func main() {
 	color := flag.String("color", "yellow", "message background color: green, red, purple, gray, random")
 	notify := flag.Bool("notify", false, "notify users")
 	format := flag.String("format", "text", "format rendered: text or html")
+	message := flag.String("message", "", "message to send")
 
 	flag.Parse()
 
 	buf := bytes.NewBuffer(nil)
 	stdin := os.Stdin
-	var message string
 	if stdin != nil {
 		pipeFile, err := stdin.Stat()
 		if err != nil {
@@ -30,12 +30,15 @@ func main() {
 			io.Copy(buf, stdin)
 			stdin.Close()
 		}
-		message = string(buf.Bytes())
+		stdin_message := string(buf.Bytes())
+		if len(stdin_message) > 0 {
+			message = &stdin_message
+		}
 	}
 
-	if len(*token) > 0 && len(*room) > 0 {
+	if len(*token) > 0 && len(*room) > 0 && len(*message) > 0 {
 		client := hipchat.NewClient(*token)
-		notifRq := &hipchat.NotificationRequest{Message: message, Color: *color, Notify: *notify, MessageFormat: *format}
+		notifRq := &hipchat.NotificationRequest{Message: *message, Color: *color, Notify: *notify, MessageFormat: *format}
 
 		resp, err := client.Room.Notification(*room, notifRq)
 		if err != nil {
@@ -43,5 +46,8 @@ func main() {
 			fmt.Fprintf(os.Stderr, "Server returns %+v\n", resp)
 			return
 		}
+	} else {
+		fmt.Fprintln(os.Stderr, "Message, Token, and Room must be specified.")
 	}
+
 }
